eea25afcbfa40c6cc68f279256919b8571848a49
ashklianko <asl@enonic.com>
2024-11-08 13:31:16 +0100

    Translate X-Data #30

--- src/main/resources/lib/content/content.ts
+++ src/main/resources/lib/content/content.ts
@@ -1,6 +1,9 @@
 import * as contentLib from '/lib/xp/content';
 import * as contextLib from '/lib/xp/context';
-import {Content, ContentType} from '/lib/xp/content';
+import * as schemaLib from '/lib/xp/schema';
+import {Content, FormItem} from '/lib/xp/content';
+
+import {XDataSchema} from '@enonic-types/lib-schema';
 
 import {TOPIC_NAME} from '../../shared/constants';
 import {isRecordEmpty} from '../utils/objects';
@@ -18,25 +21,28 @@ export function getTranslatableDataFromContent(contentId: string, context: strin
     }
 
     const flatData = flattenData(content.data);
-    const toTranslate = getTranslatableFields(flatData, contentType);
+    const dataToTranslate = getTranslatableFields(flatData, contentType.form);
 
     if (content.displayName) {
-        const topic = makeDisplayNameDataEntry(content.displayName, contentType.description);
-        toTranslate[TOPIC_NAME] = topic;
+        dataToTranslate[TOPIC_NAME] = makeDisplayNameDataEntry(content.displayName, contentType.description);
     }
 
-    return toTranslate;
+    const flatXData = flattenData(content.x as Record<string, Property>);
+    const xDataToTranslate = getXDataFieldsToTranslate(flatXData);
+
+    for (const xDataPath in xDataToTranslate) {
+        dataToTranslate[`__xdata__/${xDataPath}`] = xDataToTranslate[xDataPath];
+    }
+
+    return dataToTranslate;
 }
 
-function getTranslatableFields(
-    flatData: Record<string, PropertyValue>,
-    contentType: ContentType,
-): Record<string, DataEntry> {
+function getTranslatableFields(flatData: Record<string, PropertyValue>, form: FormItem[]): Record<string, DataEntry> {
     if (isRecordEmpty(flatData)) {
         return {};
     }
 
-    const formItemsWithPath = getPathsToTranslatableFields(contentType.form).filter(
+    const formItemsWithPath = getPathsToTranslatableFields(form).filter(
         (item: FormItemWithPath): item is InputWithPath => isInput(item),
     );
     return mapDataToFormItems(flatData, formItemsWithPath);
@@ -94,4 +100,72 @@ function makeDisplayNameDataEntry(displayName: string, context: string): DataEnt
         schemaType: 'TextLine',
         schemaLabel: context,
     };
+}
+
+/*
+ * XData fields are stored with a prefix that includes the app name and the xData name.
+ * Meanwhile the schema form that we fetch has no such prefix. We need to match the schema form fields with the actual xData fields.
+ */
+function getXDataFieldsToTranslate(xData: Record<string, PropertyValue>): Record<string, DataEntry> {
+    const result: Record<string, DataEntry> = {};
+    const schemas = getXDataSchemas(xData);
+
+    for (const schemaPrefix in schemas) {
+        const entriesByXData = getXDataEntriesBySchemaPrefix(xData, schemaPrefix);
+        const xDataItemsToTranslate = getTranslatableFields(entriesByXData, schemas[schemaPrefix].form);
+
+        for (const path in xDataItemsToTranslate) {
+            result[`${schemaPrefix}${path}`] = xDataItemsToTranslate[path];
+        }
+    }
+
+    return result;
+}
+
+// Grouping x data schemas by appName/xDataName key
+function getXDataSchemas(xData: Record<string, PropertyValue>): Record<string, XDataSchema> {
+    const schemas: Record<string, XDataSchema> = {};
+
+    for (const path in xData) {
+        const schemaPrefix = makeSchemaPrefix(path);
+
+        if (schemaPrefix && !schemas[schemaPrefix]) {
+            schemas[schemaPrefix] = getSchema(schemaPrefix);
+        }
+    }
+
+    return schemas;
+}
+
+function makeSchemaPrefix(path: string): string | undefined {
+    const pathParts = path.split('/');
+    return pathParts.length > 2 ? `${pathParts[0]}/${pathParts[1]}` : undefined;
+}
+
+function getSchema(xDataPath: string): XDataSchema {
+    const pathParts = xDataPath.split('/');
+    const schemaKey = `${pathParts[0].replace(/-/g, '.')}:${pathParts[1]}`; // replace dashes with dots
+
+    return contextLib.run(
+        {
+            principals: ['role:system.admin'],
+        },
+        () => schemaLib.getSchema({name: schemaKey, type: 'XDATA'}),
+    );
+}
+
+function getXDataEntriesBySchemaPrefix(
+    xData: Record<string, PropertyValue>,
+    schemaPrefix: string,
+): Record<string, PropertyValue> {
+    const result: Record<string, PropertyValue> = {};
+    const schemaPrefixWithSeparator = `${schemaPrefix}/`;
+
+    for (const path in xData) {
+        if (path.startsWith(schemaPrefixWithSeparator)) {
+            result[path.replace(schemaPrefixWithSeparator, '')] = xData[path];
+        }
+    }
+
+    return result;
 }
